92f6f111cfb041cbe4eef651b41e891862ffe33e
duane-wright <checkers@u.washington.edu>
2014-02-11 00:18:35 -0800

    Update microblog_tests.py

--- assignments/session05/microblog/microblog_tests.py
+++ assignments/session05/microblog/microblog_tests.py
@@ -1,8 +1,8 @@
 import os
 import tempfile
 import unittest
-import microblog
 
+import microblog
 
 class MicroblogTestCase(unittest.TestCase):
 
@@ -13,65 +13,72 @@ class MicroblogTestCase(unittest.TestCase):
         self.client = microblog.app.test_client()
         self.app = microblog.app
         microblog.init_db()
-
+        
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(microblog.app.config['DATABASE'])
-
+        
     def test_database_setup(self):
-        con = microblog.connect_db()
-        cur = con.execute('PRAGMA table_info(entries);')
-        rows = cur.fetchall()
-        self.assertEquals(len(rows), 3)
-
+    	con = microblog.connect_db()
+    	cur = con.execute('PRAGMA table_info(entries);')
+    	rows = cur.fetchall()
+    	self.assertEquals(len(rows), 3)
+    	
+    def test_login_passes(self):
+        with self.app.test_request_context('/'):
+            #microblog.do_login(microblog.app.config['admin'],
+            #                   microblog.app.config['default'])
+            microblog.do_login('admin', 'default')
+            self.assertTrue(session.get('logged_in', False))
+    
     def test_write_entry(self):
-        expected = ("My Title", "My Text")
-        with self.app.test_request_context('/'):
-            microblog.write_entry(*expected)
-            con = microblog.connect_db()
-            cur = con.execute("select * from entries;")
-            rows = cur.fetchall()
-        self.assertEquals(len(rows), 1)
-        for val in expected:
-            self.assertTrue(val in rows[0])
-
+    	expected = ("My Title", "My Text")
+    	with self.app.test_request_context('/'):
+        	microblog.write_entry(*expected)
+        	con = microblog.connect_db()
+        	cur = con.execute("select * from entries;")
+        	rows = cur.fetchall()
+    	self.assertEquals(len(rows), 1)
+    	for val in expected:
+        	self.assertTrue(val in rows[0])
+        
     def test_get_all_entries_empty(self):
-        with self.app.test_request_context('/'):
-            entries = microblog.get_all_entries()
-            self.assertEquals(len(entries), 0)
+    	with self.app.test_request_context('/'):
+        	entries = microblog.get_all_entries()
+        	self.assertEquals(len(entries), 0)
 
-    def test_get_all_entries(self):
-        expected = ("My Title", "My Text")
-        with self.app.test_request_context('/'):
-            microblog.write_entry(*expected)
-            entries = microblog.get_all_entries()
-            self.assertEquals(len(entries), 1)
-            for entry in entries:
-                self.assertEquals(expected[0], entry['title'])
-                self.assertEquals(expected[1], entry['text'])
+	def test_get_all_entries(self):
+		expected = ("My Title", "My Text")
+		with self.app.test_request_context('/'):
+			microblog.write_entry(*expected)
+			entries = microblog.get_all_entries()
+			self.assertEquals(len(entries), 1)
+			for entry in entries:
+				self.assertEquals(expected[0], entry['title'])
+				self.assertEquals(expected[1], entry['text'])
+				
+	def test_empty_listing(self):
+		actual = self.client.get('/').data
+    	expected = 'No entries here so far'
+    	self.assertTrue(expected in actual)
 
-    def test_empty_listing(self):
-        actual = self.client.get('/').data
-        expected = 'No entries here so far'
-        self.assertTrue(expected in actual)
-
-    def test_listing(self):
-        expected = ("My Title", "My Text")
-        with self.app.test_request_context('/'):
-            microblog.write_entry(*expected)
-        actual = self.client.get('/').data
-        for value in expected:
-            self.assertTrue(value in actual)
-
+	def test_listing(self):
+		expected = ("My Title", "My Text")
+    	with self.app.test_request_context('/'):
+    	    microblog.write_entry(*expected)
+    	actual = self.client.get('/').data
+    	for value in expected:
+        	self.assertTrue(value in actual)
+        	
     def test_add_entries(self):
-        actual = self.client.post('/add', data=dict(
-            title='Hello',
-            text='This is a post'
-        ), follow_redirects=True).data
-        self.assertFalse('No entries here so far' in actual)
-        self.assertTrue('Hello' in actual)
-        self.assertTrue('This is a post' in actual)
-
-
+    	actual = self.client.post('/add', data=dict(
+        	title='Hello',
+        	text='This is a post'
+    	), follow_redirects=True).data
+    	self.assertFalse('No entries here so far' in actual)
+    	self.assertTrue('Hello' in actual)
+    	self.assertTrue('This is a post' in actual)
+    	
+        
 if __name__ == '__main__':
     unittest.main()
