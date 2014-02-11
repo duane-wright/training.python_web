74f617e5cd82ca51640c872fb4396cea4d9924af
duane-wright <checkers@u.washington.edu>
2014-02-11 00:17:52 -0800

    Update microblog.py

--- assignments/session05/microblog/microblog.py
+++ assignments/session05/microblog/microblog.py
@@ -1,29 +1,24 @@
-from flask import Flask
-from flask import g
-from flask import render_template
-from flask import abort
-from flask import request
-from flask import url_for
-from flask import redirect
-import sqlite3
 from contextlib import closing
+import sqlite3
+
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
 
 app = Flask(__name__)
 
 app.config.from_pyfile('microblog.cfg')
 
-
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-
+    
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
-
+        
 def get_database_connection():
     db = getattr(g, 'db', None)
     if db is None:
@@ -36,12 +31,12 @@ def teardown_request(exception):
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
-
+        
 
 def write_entry(title, text):
     con = get_database_connection()
     con.execute('insert into entries (title, text) values (?, ?)',
-                [title, text])
+                 [title, text])
     con.commit()
 
 
@@ -49,6 +44,15 @@ def get_all_entries():
     con = get_database_connection()
     cur = con.execute('SELECT title, text FROM entries ORDER BY id DESC')
     return [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    
+
+def do_login(usr, pwd):
+    if usr != app.config['USERNAME']:
+        raise ValueError
+    elif pwd != app.config['PASSWORD']:
+        raise ValueError
+    else:
+        session['logged_in'] = True
 
 
 @app.route('/')
@@ -56,13 +60,38 @@ def show_entries():
     entries = get_all_entries()
     return render_template('show_entries.html', entries=entries)
 
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        try:
+            do_login(request.form['username'],
+                     request.form['password'])
+        except ValueError:
+            error = "Invalid Login"
+        else:
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+  
+  
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You have logged out')
+    return redirect(url_for('show_entries'))
+
 
 @app.route('/add', methods=['POST'])
 def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
     try:
         write_entry(request.form['title'], request.form['text'])
-    except sqlite3.Error:
-        abort(500)
+        flash('New entry was successfully posted')
+    except sqlite3.Error as e:
+        flash('Oops - there was an error: %s' % e.args[0])
     return redirect(url_for('show_entries'))
 
 
